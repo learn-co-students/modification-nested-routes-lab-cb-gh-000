dd97f25241ff70a6e179422dcfa8070002006a15
Rauof M <abdelrauof197@gmail.com>
2018-03-19 13:34:00 +0000

    Done.

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,3 +1,11 @@
 module SongsHelper
 
+  def artist_se_field(song)
+    if song.artist.nil?
+      text_field_tag "song[artist_name]"
+    else
+      hidden_field_tag "song[artist_id]", song.artist_id
+    end
+  end
+
 end
